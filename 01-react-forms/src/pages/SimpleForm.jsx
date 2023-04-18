5cb58122b59041be08b5da315fcdebcada504d95
César Guerra <warderer@gmail.com>
2023-04-17 20:38:00 -0600

    ✨ SimpleForm terminado

--- 01-react-forms/src/pages/SimpleForm.jsx
+++ 01-react-forms/src/pages/SimpleForm.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import logo from '../assets/react.svg'
 
 const SimpleForm = () => {
+  // Paso #2: Crear los estados donde guardaremos los datos del formulario
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  // Paso #4: Crear la función que se ejecutara cuando se envie el formulario
+  const handleSubmit = (event) => {
+    event.preventDefault() // Evita que se recargue la página
+    const submittedData = JSON.stringify({
+      email,
+      password
+    })
+    console.log(submittedData)
+  }
+
+  // Paso #1: Crear el formulario base
   return (
     <div className='login'>
       <div className='login-container'>
         <img src={logo} alt='logo' />
 
-        <form onSubmit={() => {}/* HANDLE SUBMIT */}>
+        <form onSubmit={handleSubmit}>
+          {/* Paso #3: Guardar cada cambio del input en su estado */}
           <label htmlFor='email'>Email</label>
           <input
             type='text'
             name='email'
             placeholder='correo@mail.com'
             id='simple-email'
-            onChange={() => {}/* HANDLE INPUT */}
+            onChange={(event) => setEmail(event.target.value)}
           />
 
           <label htmlFor='password'>Password</label>
@@ -21,7 +38,7 @@ const SimpleForm = () => {
             type='password'
             name='password'
             id='simple-password'
-            onChange={() => {}/* HANDLE INPUT */}
+            onChange={(event) => setPassword(event.target.value)}
           />
 
           <button type='submit'>
