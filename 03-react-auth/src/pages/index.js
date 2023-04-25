c78a12be427e6fe4af3fcb05ab14bbc5571ef792
César Guerra <warderer@gmail.com>
2023-04-24 20:52:46 -0600

    ✨ Configuración del proyecto base

---
+++ 03-react-auth/src/pages/index.js
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard'
+import Login from './Login'
+import Signup from './Signup'
+import Secret from './Secret'
+import Home from './Home'
+
+export {
+  Dashboard,
+  Login,
+  Signup,
+  Secret,
+  Home
+}
