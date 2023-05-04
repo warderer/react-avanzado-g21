47da038c147838afe6bd01749bab0f8829c4c3f6
César Guerra <warderer@gmail.com>
2023-05-03 19:53:03 -0600

    ✏️ Fix Import de AuthProvider

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import Header from '@/components/Header'
 import RoutesIndex from '@/routes/Index'
-import AuthProvider from '@/context/AuthContext'
+import { AuthProvider } from '@/context/AuthContext'
 import './App.css'
 
 function App () {
