b599496defb46197647df8565a8a13b4a0adb421
César Guerra <warderer@gmail.com>
2023-05-03 21:15:10 -0600

    ✨ Dashboard con info del usuario

--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -2,10 +2,23 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3000'
 
+// Si existe un token, mandalo en la petición
+axios.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getSingleUser = (id) => axios.get(`${BASE_URL}/users/${id}`)
 
 export {
   registerUserService,
-  loginUserService
+  loginUserService,
+  getSingleUser
 }
