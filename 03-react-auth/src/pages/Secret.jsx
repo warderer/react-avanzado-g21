9a3479483ce4001c6d056de258ace0e43abbd8b3
César Guerra <warderer@gmail.com>
2023-05-03 20:45:09 -0600

    ✨ Render Condicional de Tipo de Usuario

--- 03-react-auth/src/pages/Secret.jsx
+++ 03-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,22 @@
+import { useAuthContext } from '@/context/AuthContext'
+
 const Secret = () => {
+  const { userPayload } = useAuthContext()
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      {
+        userPayload?.role === 'ADMIN'
+          ? <h2>Hola Admin</h2>
+          : <h2>Hola User</h2>
+      }
+
+      {userPayload?.role === 'CUSTOMER' &&
+        <h2>Esto solo lo ve el CUSTOMER</h2>}
+
+      {userPayload?.role === 'ADMIN' &&
+        <h2>Esto solo lo ve el ADMIN</h2>}
+    </>
   )
 }
 export default Secret
