bbfe9a6dd17262f89eddc5d6a84a954bc8b44514
César Guerra <warderer@gmail.com>
2023-05-03 20:35:11 -0600

    ✨ Protegiendo Rutas con Auth

--- 03-react-auth/src/routes/Index.jsx
+++ 03-react-auth/src/routes/Index.jsx
@@ -1,11 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from '@/context/AuthContext'
 import { Home, Dashboard, Login, Signup, Secret } from '@/pages'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route
+        path='/dashboard'
+        element={
+          isAuth
+            ? <Dashboard />
+            : <Navigate to='/login' replace />
+      }
+      />
       <Route path='/secret' element={<Secret />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
